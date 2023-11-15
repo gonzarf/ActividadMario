9b8d082ead99c708dcd4f4bb80ab2b43332bd193
gonzarf <gonzarf21@gmail.com>
2023-11-15 14:22:21 +0100

    Tema añadido

--- Actividad12/app/src/main/java/com/example/actividad1/SplashScreen.java
+++ Actividad12/app/src/main/java/com/example/actividad1/SplashScreen.java
@@ -23,18 +23,16 @@ public class SplashScreen extends AppCompatActivity {
         openLogin();
 
         ImageView logoSplash = findViewById(R.id.logoSplash);
-        TextView appNameSplash = findViewById(R.id.appNameSplash);
 
         Animation fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        appNameSplash.startAnimation(fadeAnimation);
         logoSplash.startAnimation(fadeAnimation);
 
         ImageView background = findViewById(R.id.backgroundSplash);
-        Glide.with(this)
+        /*Glide.with(this)
                 .load("https://images.unsplash.com/photo-1533240332313-0db49b459ad6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80")
                 .transition(DrawableTransitionOptions.withCrossFade(3000))
                 .centerCrop()
-                .into(background);
+                .into(background);*/
     }
 
     public void openLogin(){
